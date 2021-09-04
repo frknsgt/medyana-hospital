39021dc8246914fce3fd6a1ae95bb40bd652325d
Furkan Söğüt <48659931+frknsgt@users.noreply.github.com>
2021-09-04 21:01:48 +0300

    feat: pagination component

---
+++ src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+})
+export class PaginationComponent implements OnInit {
+  constructor() {}
+
+  @Input() config: PaginationInstance = {
+    id: 'custom',
+    itemsPerPage: 10,
+    currentPage: 1,
+  };
+
+  ngOnInit(): void {}
+}
